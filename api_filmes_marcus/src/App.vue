0daeec910b95845919980cfe015ef37bcf7a3c7f
M4rcua <marcus.oliveira2403@gmail.com>
2022-10-18 09:06:28 -0300

    Atualização 1810

---
+++ api_filmes_marcus/src/App.vue
@@ -0,0 +1,30 @@
+<script>
+import ObjetoView from "./views/ObjetoView.vue";
+export default {
+  components: { ObjetoView },
+};
+</script>
+
+<template>
+  <ObjetoView />
+
+  <main>
+    <div class="container">
+      <img src="./assets/jamalzinholindo.jpg" alt="jamal murray" />
+      <img src="./assets/jamalzinholindo.jpg" alt="jamal murray" />
+      <img src="./assets/jamalzinholindo.jpg" alt="jamal murray" />
+    </div>
+  </main>
+</template>
+
+<style>
+.container {
+  display: flex;
+}
+
+img {
+  width: 500px;
+  height: 500px;
+  margin: 20px;
+}
+</style>
---
+++ api_filmes_marcus/src/views/ObjetoView.vue
@@ -0,0 +1,21 @@
+<template>
+  <header>
+    <i><h1>VE Videos</h1></i>
+    <i><h1>Séries</h1></i>
+    <i><h1>Filmes</h1></i>
+    <i><h1>Sobre nós</h1></i>
+  </header>
+</template>
+
+<style>
+header {
+  display: flex;
+}
+
+h1 {
+  font-family: "Open Sans", sans-serif;
+  color: rgb(95, 95, 93);
+  padding: 40px;
+  padding-top: 0px;
+}
+</style>
